c20841de475127491ba4b0bd30cc0e78aeb96472
asyinka <asyinka@gmail.com>
2022-08-07 12:35:58 +0100

    completed the javaScript assigned

---
+++ 02-counter/app.js
@@ -0,0 +1,44 @@
+let decreaseBtn = document.querySelector("#btnDecrease");
+let increaseBtn = document.querySelector("#btnIncrease");
+let resetBtn = document.querySelector("#btnReset");
+// let count = document.getElementById("value").innerText;
+let count = 0;
+let valueStyle = document.getElementById("value").style;
+
+//functions to effect action on click of buttons
+decreaseBtn.addEventListener("click", countDecrease);
+increaseBtn.addEventListener("click", increaseCount);
+resetBtn.addEventListener("click", resetCount);
+
+
+//function to decrease count by 1 and invoke color change function
+function countDecrease(){
+    count -=1;
+    document.getElementById("value").innerHTML = count;
+    colorChanges();
+}
+
+//function to increase count by 1 and invoke color change function
+function increaseCount(){
+    count +=1;
+    document.getElementById("value").innerHTML = count;
+    colorChanges();
+}
+
+//function to reset count and invoke color changes
+function resetCount(){
+    count = 0;
+    document.getElementById("value").innerHTML = count;
+    colorChanges();
+}
+
+//function for color changes effect
+function colorChanges(){
+    if(count === 0){
+        valueStyle.color = "black";
+    } else if (count < 0){
+        valueStyle.color = "red";
+    } else {
+        valueStyle.color = "green";
+    }
+}
